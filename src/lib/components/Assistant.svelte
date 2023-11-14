276762cc13939b1669c2b4cb48440629d1e3fa19
Lucas Boganin <39099403+Spacelocust@users.noreply.github.com>
2023-11-14 14:00:03 +0000

    feat: add toast for Carlos error [#9]

--- src/lib/components/Assistant.svelte
+++ src/lib/components/Assistant.svelte
@@ -5,6 +5,7 @@
 
   import { prefersReducedMotion } from '$lib/utils/preferences';
   import { requestAnimationFrame } from '$lib/utils/request-animation-frame';
+  import { toasts } from '$lib/utils/toats';
 
   import Card from './Card.svelte';
 
@@ -104,7 +105,7 @@
     const question = formData.get('question');
 
     if (typeof question !== 'string' || question.trim() === '') {
-      // TODO: handle error
+      toasts.error('Veuillez saisir une question');
 
       carlosStatus = 'available';
 
@@ -135,9 +136,7 @@
     });
 
     if (!response.ok || !response.body) {
-      console.error('Carlos is not available...', response);
-
-      // TODO: handle error
+      toasts.error('Carlos n\'est pas disponible');
 
       return;
     }
@@ -149,9 +148,7 @@
 
       await handleMessageChunk(reader);
     } catch (error) {
-      console.error('Carlos is not available...', error);
-
-      // TODO: handle error
+      toasts.error('Carlos n\'est pas disponible');
     }
 
     abortController = null;
